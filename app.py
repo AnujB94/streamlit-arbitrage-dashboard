8ca64991eaca3a377bf7ee4610bc77076c8be444
AnujB94 <anujb0904@gmail.com>
2025-07-22 14:01:13 +0530

    Without Refresh v1

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if not is_market_open():
 
 all_fno = ['NIFTY', 'NIFTYIT', 'BANKNIFTY', 'MANAPPURAM', 'MAXHEALTH', 'ICICIGI', 'OIL', 'JUBLFOOD', 'GLENMARK', 'BEL', 'ASTRAL', 'MUTHOOTFIN', 'SUPREMEIND', 'HAL', 'BDL', 'BLUESTARCO', 'ONGC', 'POLICYBZR', 'BIOCON', 'SOLARINDS', 'GODREJPROP', 'CDSL', 'TORNTPOWER', 'PRESTIGE', 'DLF', 'MCX', 'OFSS', 'CHAMBLFERT', 'TECHM', 'OBEROIRLTY', 'MFSL', 'PIIND', 'LODHA', 'APLAPOLLO', 'COFORGE', 'DELHIVERY', 'SBICARD', 'KALYANKJIL', 'PPLPHARMA', 'BHARATFORG', 'WIPRO', 'TCS', 'CIPLA', 'BOSCHLTD', 'LTIM', 'TVSMOTOR', 'NBCC', 'EICHERMOT', 'TATACOMM', 'MARUTI', 'PAGEIND', 'SRF', 'ALKEM', 'CROMPTON', 'APOLLOHOSP', 'INDIANB', 'NYKAA', 'LAURUSLABS', 'SUNPHARMA', 'KAYNES', 'DRREDDY', 'SHRIRAMFIN', 'BHEL', 'CUMMINSIND', 'UPL', 'IRCTC', 'KEI', 'GAIL', 'SIEMENS', 'MAZDOCK', 'ASIANPAINT', 'MARICO', 'BRITANNIA', 'TRENT', 'TATAMOTORS', 'BHARTIARTL', 'HCLTECH', 'SHREECEM', 'INFY', 'NESTLEIND', 'ASHOKLEY', 'BAJFINANCE', 'JIOFIN', 'HINDCOPPER', 'TORNTPHARM', 'HDFCLIFE', 'ABB', 'BALKRISIND', 'COALINDIA', 'CGPOWER', 'BANKBARODA', 'TATASTEEL', 'POLYCAB', 'HINDZINC', 'TATACONSUM', 'PERSISTENT', 'ICICIPRULI', 'MANKIND', 'PATANJALI', 'FEDERALBNK', 'M&M', 'NTPC', 'LICI', 'VEDL', 'RECLTD', 'SAIL', 'AXISBANK', 'ZYDUSLIFE', 'LT', 'KPITTECH', 'ICICIBANK', 'HINDUNILVR', 'SBILIFE', 'CESC', 'HEROMOTOCO', 'NATIONALUM', 'NAUKRI', 'AUROPHARMA', 'PHOENIXLTD', 'LICHSGFIN', 'BAJAJFINSV', 'DMART', 'MOTHERSON', 'MPHASIS', 'IDFCFIRSTB', 'VOLTAS', 'AMBUJACEM', 'INDUSTOWER', 'DIVISLAB', 'TATAPOWER', 'PETRONET', 'ETERNAL', 'DALBHARAT', 'JSWSTEEL', 'UNOMINDA', 'YESBANK', 'ACC', 'GRASIM', 'FORTIS', 'PIDILITIND', 'BSOFT', 'TATACHEM', 'ULTRACEMCO', 'TITAN', 'TATATECH', 'HAVELLS', 'RELIANCE', 'ABCAPITAL', 'KOTAKBANK', 'BAJAJ-AUTO', 'IRB', 'M&MFIN', 'IDEA', 'HUDCO', 'POWERGRID', 'LTF', 'ABFRL', 'TITAGARH', 'GMRAIRPORT', 'DABUR', 'DIXON', 'IRFC', 'POONAWALLA', 'CYIENT', 'BSE', 'SJVN', 'CONCOR', 'PAYTM', 'BANKINDIA', 'VBL', 'RVNL', 'INDHOTEL', 'SYNGENE', 'HDFCBANK', 'TATAELXSI', 'LUPIN', 'PNB', 'TIINDIA', 'AUBANK', 'INDUSINDBK', 'GODREJCP', 'COLPAL', 'HDFCAMC', 'ADANIENT', 'BANDHANBNK', 'HINDALCO', 'RBLBANK', 'IIFL', 'SONACOMS', 'NHPC', 'JSWENERGY', 'SBIN', 'JSL', 'HINDPETRO', 'AARTIIND', 'ADANIENSOL', 'MGL', 'ITC', 'IOC', 'CHOLAFIN', 'IEX', 'IGL', 'BPCL', 'PEL', 'HFCL', 'INOXWIND', 'ATGL', 'JINDALSTEL', 'CAMS', 'ADANIGREEN', 'UNITDSPR', 'PFC', 'NCC', 'EXIDEIND', 'UNIONBANK', 'NMDC', 'ADANIPORTS', 'PNBHOUSING', 'GRANULES', 'ANGELONE', 'CANBK', 'INDIGO', 'IREDA']
 default_stocks = ["INFY", "TCS", "RELIANCE", "HDFCBANK", "ITC", "ICICIBANK"]
-symbols = st.multiselect("Select Stocks", sorted(all_fno[3:]), default= default_stocks)
+symbols = st.multiselect("Select Stocks", sorted(all_fno[3:]), default=sorted(all_fno[3:]) )
 
 today = datetime.today()
 from_date = (today - timedelta(days=7)).strftime("%d-%m-%Y")
@@ -66,7 +66,7 @@ def process_symbol(symbol):
 with st.spinner("⏳ Awaiting data..."):
     def update_data():
         results = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=70) as executor:
             futures = [executor.submit(process_symbol, symbol) for symbol in symbols]
             for f in concurrent.futures.as_completed(futures):
                 result = f.result()
